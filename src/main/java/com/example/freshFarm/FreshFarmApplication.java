867d8d56e470410296c1c6f7c91dcf6666cad5ad
Magdalena Janeckova <magdalena.janeckova92@gmail.com>
2025-08-05 11:44:17 +0100

    Initial set-up

---
+++ src/main/java/com/example/freshFarm/FreshFarmApplication.java
@@ -0,0 +1,13 @@
+package com.example.freshFarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshFarmApplication.class, args);
+	}
+
+}
