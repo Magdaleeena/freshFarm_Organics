e432176288f4fc0b9d7de23f83b1d98a612f530a
Magdalena Janeckova <magdalena.janeckova92@gmail.com>
2025-08-11 16:57:43 +0100

    cheese & yoghurt

--- src/main/java/com/example/freshFarm/controller/ProductController.java
+++ src/main/java/com/example/freshFarm/controller/ProductController.java
@@ -6,7 +6,6 @@ import com.example.freshFarm.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @Controller
@@ -19,15 +18,14 @@ public class ProductController {
     public String fruits(Model model){
         List<Product> fruits = productRepository.findByCategoryIgnoreCase("fruit");
         model.addAttribute("fruits", fruits);
-        return "fruits";
+        return "/products/fruits";
     }
 
     @GetMapping("/vegetables")
-    public ModelAndView vegetables(){
+    public String vegetables(Model model){
         List<Product> vegetables = productRepository.findByCategoryIgnoreCase("vegetable");
-        ModelAndView vegView = new ModelAndView("vegetables");
-        vegView.addObject("vegetables", vegetables);
-        return vegView;
+        model.addAttribute("vegetables", vegetables);
+        return "products/vegetables";
     }
 
     @GetMapping("/milk")
@@ -35,5 +33,19 @@ public class ProductController {
         List<Product> milk = productRepository.findByCategoryIgnoreCase("milk");
         model.addAttribute("milk", milk);
         return "products/milk";
+    }
+
+    @GetMapping("/cheese")
+    public String cheese(Model model){
+        List<Product> cheese = productRepository.findByCategoryIgnoreCase("cheese");
+        model.addAttribute("cheese", cheese);
+        return "products/cheese";
+    }
+
+    @GetMapping("/yoghurt")
+    public String yoghurt(Model model){
+        List<Product> yoghurt = productRepository.findByCategoryIgnoreCase("yoghurt");
+        model.addAttribute("yoghurt", yoghurt);
+        return "products/yoghurt";
     }
 }
